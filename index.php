a11a8a1276efa7dec466de65de9a819d8b077c92
cst-d3 <jchee12@my.bcit.ca>
2015-01-08 15:02:55 -0800

    Added 3rd student and ksort

--- index.php
+++ index.php
@@ -12,9 +12,15 @@ and open the template in the editor.
     <body>
         <?php
             include('Student.php');
+            // Justin Chee
+            // A00831111
+            // COMP4711
+            // Lab01
             
+            // An array of Students
             $students = array();
             
+            // First student
             $first = new Student();
             $first->surname = "Doe";
             $first->first_name = "John";
@@ -25,6 +31,7 @@ and open the template in the editor.
             $first->add_grade(55);
             $students['j123'] = $first;
             
+            // Second student
             $second = new Student();
             $second->surname = "Einstein";
             $second->first_name = "Albert";
@@ -36,8 +43,21 @@ and open the template in the editor.
             $second->add_grade(50);
             $students['a456'] = $second;
             
+            // Third student
+            $third = new Student();
+            $third->surname = "Chee";
+            $third->first_name = "Justin";
+            $third->add_email("home", "chee_justin@hotmail.com");
+            $third->add_email("school", "jchee12@my.bcit.ca");
+            $third->add_grade(95);
+            $third->add_grade(86);
+            $third->add_grade(100);
+            $students['j888'] = $third;
+            
+            // Sort
             ksort($students);
             
+            // Print each student in the array
             foreach ($students as $student)
                 echo $student->toString();
         ?>
